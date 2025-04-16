48c0fb25552ec2ec8194f2e398f2c1c98a572037
00018441 <wiut.00018441@yandex.com>
2025-04-16 16:27:25 +0500

    cc csconfig: send messages (not)securely

--- src/shared/utils/sign-success-message-cookie.util.js
+++ src/shared/utils/sign-success-message-cookie.util.js
@@ -1,7 +1,6 @@
 export function signSuccessMessageCookie(res, message) {
     res.clearCookie("successMessage");
     res.cookie("successMessage", message, {
-        httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 15000,
     });
